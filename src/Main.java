d6182bc041031a7ebf1faf4864a4cb13ce952974
Aksellercan <akselercan2003@gmail.com>
2025-08-26 16:14:28 +0300

    refactor

--- src/Main.java
+++ src/Main.java
@@ -8,32 +8,26 @@ public class Main {
         }
         if (args.length <= 10) {
             if (args[0].equals("start")) {
-                System.out.println(":: Ctrl+C to stop the server");
                 int port = 0;
                 int backlog = 10;
                 String name = null;
                 File sourceFolder = null;
-                try {
-                    for (int i = 0; i < args.length; i++) {
-                        if (args[i].equals("--name")) {
-                            name = args[i+1];
-                        }
-                        if (args[i].equals("--path")) {
-                            sourceFolder = new File(args[i + 1]);
-                        }
-                        if (args[i].equals("--port")) {
-                            port = Integer.parseInt(args[i + 1]);
-                        }
-                        if (args[i].equals("--backlog")) {
-                            backlog = Integer.parseInt(args[i + 1]);
-                        }
-                        if (args[i].equals("--verbose")) {
-                            Logger.setDebugOutput(true);
-                        }
+                for (int i = 0; i < args.length; i++) {
+                    if (args[i].equals("--name")) {
+                        name = args[i+1];
                     }
-                } catch (Exception e) {
-                    System.err.println("Invalid Usage\nUsage: HTTPServer start --path <path> --port <port> --backlog <backlog> --verbose");
-                    return;
+                    if (args[i].equals("--path")) {
+                        sourceFolder = new File(args[i + 1]);
+                    }
+                    if (args[i].equals("--port")) {
+                        port = Integer.parseInt(args[i + 1]);
+                    }
+                    if (args[i].equals("--backlog")) {
+                        backlog = Integer.parseInt(args[i + 1]);
+                    }
+                    if (args[i].equals("--verbose")) {
+                        Logger.setDebugOutput(true);
+                    }
                 }
                 HTTPServer server = new HTTPServer(port, backlog);
                 if (sourceFolder != null) {
